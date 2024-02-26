25bfc7588e2b272fcb10f0cd3e6e1d62feaebcdf
nikhilprojectwork <githubnikhilrepo@gmail.com>
2024-02-26 14:38:20 +0530

    Adding New Loop

--- new.js
+++ new.js
@@ -2,3 +2,7 @@ let brn = document.getElementsByClassName("key");
 brn.addEventListner("click" , ()=>{
     console.log("Button was clicked")
 })
+
+for(let i =  0 ; i<= 10 ; i++){
+    console.log("Hello Nikhil Developer")
+}
